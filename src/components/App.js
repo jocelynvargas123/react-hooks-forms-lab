ab4d9c28b234c62653cd0b92421d347ca7c09f92
Jocelyn <jocelynvargas123@yahoo.com>
2023-08-31 23:53:38 -0700

    Lab Finished

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,14 @@ function App() {
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
+  function handleSubmitForm(e) {
+    setItems([...items, e])
+  }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit ={handleSubmitForm} />
     </div>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React,{useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+  const [name, setNameSubmit] = useState("");
+  const [category, setFormSubmit] = useState("Produce");
+
+  function nameChange(event){
+    setNameSubmit(event.target.value);
+  }
+  function formChange(event){
+    setFormSubmit(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    onItemFormSubmit({
+      id: uuid(),
+      name,
+      category,
+    })
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
-        Name:
-        <input type="text" name="name" />
+        Name: 
+        <input type="text" name="name" value={name} onChange={nameChange}/>
       </label>
 
       <label>
-        Category:
-        <select name="category">
+        Category: 
+        <select name="category" value={category} onChange={formChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" >Add to List</button>
     </form>
   );
 }
